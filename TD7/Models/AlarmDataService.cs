1703f733b71d972169abd9cea1719ac91212882e
Thibault JP <thibault60000@gmail.com>
2018-04-08 11:37:53 +0200

    Ajout Fonction Creation de Reveil au lieu de faire ça en dur

--- TD7/Models/AlarmDataService.cs
+++ TD7/Models/AlarmDataService.cs
@@ -10,33 +10,51 @@ namespace TD7.Models
     {
         // List d'alarmes 
         private List<Alarm> alarmes = null;
+        private int arraySize;
 
         public async Task<Alarm[]> GetAlarmAsync()
-        {
+        {             
             return await Task.Run(() =>
             {
                 alarmes = new List<Alarm>();
-                alarmes.Add(new Alarm()
-                {
-                    Cle = 1,
-                    Titre = "Alarme 1",
-                    Date = new DateTime(2008, 3, 1, 7, 0, 0),
-                    Repetition = 2
-                });
+                DateTime Date = new DateTime(2008, 3, 1, 7, 0, 0);
+                AddAlarmSync("Mon Alarme", Date, 2);
 
                 // console.WriteLine(date1.ToString("F", new System.Globalization.CultureInfo("fr-FR")));
-
-                    // Displays samedi 1 mars 2008 07:00:00
+                // Displays samedi 1 mars 2008 07:00:00
                 return alarmes.ToArray();
             });
-
         }
+
 
         public async Task<Alarm> GetAlarmAsync(int cleAlarm)
         {
             return await Task.Run(() =>
             {
                 return alarmes.ToArray().FirstOrDefault(i => i.Cle == cleAlarm);
+            });
+        }
+
+        public async void AddAlarmSync(string titre, DateTime date, int repetition)
+        {
+            if (alarmes.ToArray().Length == 0)
+            {
+                this.arraySize = 1;
+            }
+            else
+            {
+                this.arraySize = alarmes.ToArray().Length + 1;
+            }
+            await Task.Run(() =>
+            {
+
+                alarmes.Add(new Alarm()
+                {
+                    Cle = arraySize,
+                    Titre = titre,
+                    Date = date,
+                    Repetition = repetition
+                });
             });
         }
 
